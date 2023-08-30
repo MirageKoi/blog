14f46842a27b01ae7496b37dd0b11537ac252df5
Denys <108667781+MirageKoi@users.noreply.github.com>
2023-08-30 13:19:10 +0300

    add views, url. upd admin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -28,7 +28,9 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-publish"]
-        indexes = [models.Index(fields=["-publish"]),]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
 
     def __str__(self) -> str:
         return self.title
