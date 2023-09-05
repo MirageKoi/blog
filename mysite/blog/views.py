20a1d137d92282aa85945d7e20d5186fe70e3505
Denys <108667781+MirageKoi@users.noreply.github.com>
2023-09-05 09:37:07 +0300

    add implementation of comment

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,10 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 
-from .models import Post
+from .forms import CommentForm
+from .models import Comment, Post
 
 
 def post_list(request):
@@ -24,5 +26,18 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, "blog/post/detail.html", {"post": post, "comments": comments, "form": form})
 
-    return render(request, "blog/post/detail.html", {"post": post})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, "blog/post/comment.html", {"post": post, "form": form, "comment": comment})
